b43c2b50e13c5bdeca4b97adb84d9d2fd03a19da
informalboi <733416@student.dusd.net>
2026-02-25 11:11:12 -0800

    getbutton finished

--- P4RomanScriptingProject/Assets/scripts/GetButton.cs
+++ P4RomanScriptingProject/Assets/scripts/GetButton.cs
@@ -13,6 +13,26 @@ public class GetButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+
+        if (down)
+
+            Debug.Log("Get Button Down is: " + down);
+
+        else if (held)
+
+            Debug.Log("Get Button held is: " + held);
+
+        else if (up)
+
+            Debug.Log("Get Button Up is: " + up);
+
+        else
+
+            Debug.Log("Get Button Down is: " + down);
+        Debug.Log("Get Button held is: " + held);
+        Debug.Log("Get Button Up is: " + up);
     }
 }
--- P4RomanScriptingProject/Assets/scripts/GetKey.cs
+++ P4RomanScriptingProject/Assets/scripts/GetKey.cs
@@ -13,6 +13,41 @@ public class GetKey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool down = Input.GetKeyDown(KeyCode.Space);
+        bool held = Input.GetKey(KeyCode.Space);
+        bool up = Input.GetKeyUp(KeyCode.Space);
+        if (down)
+
+            Debug.Log("Get Key Down is: " + down);
+
+        else if (held)
+
+            Debug.Log("Key held is: " + held);
+
+        else if (up)
+
+            Debug.Log("Get Key Up is: " + up);
+
+        else
+
+            Debug.Log("Get Key Down is: " + down);
+        Debug.Log("Key held is: " + held);
+        Debug.Log("Get Key Up is: " + up); if (down)
+
+            Debug.Log("Get Key Down is: " + down);
+
+        else if (held)
+
+            Debug.Log("Key held is: " + held);
+
+        else if (up)
+
+            Debug.Log("Get Key Up is: " + up);
+
+        else
+
+            Debug.Log("Get Key Down is: " + down);
+        Debug.Log("Key held is: " + held);
+        Debug.Log("Get Key Up is: " + up);
     }
 }
