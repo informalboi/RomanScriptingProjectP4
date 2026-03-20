4c93e1e37a6f62ac07e881faf8b06309c935840f
informalboi <733416@student.dusd.net>
2026-03-19 21:48:53 -0700

    new

--- P4RomanScriptingProject/Assets/scripts/AxisRawExample.cs
+++ P4RomanScriptingProject/Assets/scripts/AxisRawExample.cs
@@ -2,21 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class AxisExample : MonoBehaviour
+public class AxisRawExample : MonoBehaviour
 {
     public float range;
     public Text textOutput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
     void Update()
     {
-        float h = Input.GetAxis("Horizontal");
+        float h = Input.GetAxisRaw("Horizontal");
         float xPos = h * range;
 
         transform.position = new Vector3(xPos, 2f, 0);
